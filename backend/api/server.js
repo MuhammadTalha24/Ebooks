ce0f00e19bb1a2cb72119ce1b9419080d49513d7
Muhammad Talha <talhaji880@gmail.com>
2024-12-25 01:58:10 +0500

    code push

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import env from 'dotenv'
 import cors from 'cors'
-import userRoutes from './routes/user.routes.js'
-import bookRoutes from './routes/book.routes.js'
+import userRoutes from '../routes/user.routes.js'
+import bookRoutes from '../routes/book.routes.js'
 import cookieParser from "cookie-parser";
-import dbConnection from './database/dbConnection.js'
+import dbConnection from '../database/dbConnection.js'
 import path from 'path';
 
 env.config()
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT
 
 
-const _dirname = path.resolve()
+
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,  // Set this to your frontend URL
@@ -30,11 +30,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/user', userRoutes)
 app.use('/api/v1/book', bookRoutes)
 
-
-app.use(express.static(path.join(_dirname, "/frontend/dist")))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
-})
 dbConnection().then(() => {
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
